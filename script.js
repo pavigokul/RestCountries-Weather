9e3074e9f294c5eca49424ac0b8d72d1c9d168ed
pavithran <130632905+pavigokul@users.noreply.github.com>
2023-05-31 17:28:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+let API = "https://restcountries.com/v3.1/all";
+
+
+let fet = fetch(API)
+  .then((response) => response.json())
+  .then((data) => {
+    
+    data.map((value) => {
+      let spreadOperator = {
+        ...value,
+        name: value.name.common,
+        flag: value.flags.png,
+        code: value.cioc,
+        capital: value.capital,
+        region: value.region,
+        latitude: value.latlng[0],
+        longitude: value.latlng[1]
+
+      };
+      createcountry(spreadOperator);
+      
+    })
+  })
+  
+  
+   
+function createcountry({ name, flag, code, capital, region,latitude,longitude }) {
+   
+  document.body.innerHTML += 
+  ` <div class="container">
+    <div class="card"  >
+    <h2 id="title" class="text-center">${name}</h2>
+    <img src="${flag}" class="flag" alt="${name}'Flag image">
+ 
+  <div class="card-body car" >
+  <p><span>Captial :</span> ${capital}</p>
+  <p><span>Region :</span> ${region}</p>
+  <p><span>Country Code :</span>${code}</p>
+  <a href="#" class="btn btn-primary" onclick=(block(${latitude},${longitude},${name})) >Click for Weather</a>
+ <div id=${name}>   </div>
+  </div>
+</div>
+</div>
+`
+};
+
+
+function block(lat,lng,name){
+
+  let WAPI =`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=36356e624b0855191b1fb6e349d06bf1`
+   
+  console.log(name)
+ fetch(WAPI)
+ .then((response) => response.json())
+ .then((data)=> {
+
+     alert(`
+               For ${name.id}  
+     Current Humidity is ${data.main.humidity}
+     Current Pressure is ${data.main.pressure}
+     Current Temperature is ${data.main.temp}`)
+    });
+};
+  
+document.addEventListener("click",(event) => event.preventDefault());
